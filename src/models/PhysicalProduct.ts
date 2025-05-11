343a0210cbff61ac42a77c8e961abb936dddd676
Sheikh Jabed <jabedbd2295@gmail.com>
2025-05-11 21:45:32 +0600

    Physical Product and Digital Product named Object's Activities Completed

--- src/models/PhysicalProduct.ts
+++ src/models/PhysicalProduct.ts
@@ -1,19 +1,8 @@
+import { Purchasable } from "../interfaces/";
 import { Product } from "./Product";
 
-export class DigitalProduct extends Product {
-  // downloadSizeMB: number;
-  constructor(
-    id: number,
-    name: string,
-    price: number,
-    quantity: number,
-    public downloadSizeMB: number
-  ) {
-    super(id, name, price, quantity);
-    // this.downloadSizeMB = downloadSizeMB;
-  }
-
-  getDownloadInfo() {
-    return `${this.name} is downloading. Size: ${this.downloadSizeMB}`;
+export class PhysicalProduct extends Product implements Purchasable {
+  getStockValue(): number {
+    return this.price;
   }
 }
