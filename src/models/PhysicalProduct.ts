b7e6da7166440744e4aaa7807d53f812488781e9
Sheikh Jabed <jabedbd2295@gmail.com>
2025-05-18 23:49:29 +0600

    Product Related Class and Methods Created and Polymorphism Related Activities Completed

--- src/models/PhysicalProduct.ts
+++ src/models/PhysicalProduct.ts
@@ -1,8 +1,6 @@
 import { Purchasable } from "../interfaces/PolymorphismInterface";
 import { Product } from "./Product";
 
-
-
 export class PhysicalProduct extends Product implements Purchasable {
   getStockValue(): number {
     return this.price;
